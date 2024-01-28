eb84cc3e88547c65b6df66b2be03f3e6b5baf40c
Janna <1737316278@qq.com>
2024-01-29 02:21:23 +0800

    优化了异常处理，删减了不必要的文件

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 const express=require("express");
 const db=require("./core/mysql");
-const route=require("./core/http")
 
 const server=express();
-var request = require('request');
 
-server.get("/getCats",async(request,response)=>{
-    let sql="SELECT `catName` FROM `Categories` ORDER BY `catId`;";
-    let result = await db.query(sql,null);
-    result = result.map((item)=>item.catName)
-    //返回数据
-    console.log(result);
-    response.json(result);
-})
+server.get("/getCats", async (request, response) => {
+    let sql = "SELECT `catName` FROM `Categories` ORDER BY `catId`;";
+
+    let result;
+    try {
+        result = await db.query(sql, null);
+        result = result.map((item) => item.catName);
+        
+        //返回数据
+        response.json({ code: 200, msg: "查询目录成功", data: result });
+    } catch (err) {
+        response.json({ code: 500, msg: "服务器错误" });
+    }
+});
 
 server.get("/getPlaces",async(request,response)=>{
     let catName = request.query.catName;
     let parmas=[catName];
-    let sql="SELECT * FROM `Places` WHERE `catName`= ? ;";
+    let sql="SELECT plId, catName, plName, longitude, latitude, time, imgUrl FROM `Places` WHERE `catName`= ? ;";
 
-    let result = await db.query(sql,parmas);
-    //返回数据
-    console.log(result);
-    response.json(result);
+    let result;
+    try{
+        result = await db.query(sql,parmas);
+
+        //返回数据
+        response.json({ code: 200, msg: "查询分类下地点成功", data: result });
+    } catch (err) {
+        response.json({ code: 500, msg: "服务器错误" });
+    }
+
 })
 
 server.get("/getDetail",async(request,response)=>{
@@ -30,21 +40,30 @@ server.get("/getDetail",async(request,response)=>{
     let parmas=[plName];
     let sql="SELECT * FROM `Places` WHERE `plName`= ? ;";
 
-    let result = await db.query(sql,parmas);
-    //返回数据
-//    console.log(parmas);
-//    console.log(result);
-    response.json(result[0]);
+    let result;
+    try{
+        result = await db.query(sql,parmas);
+
+        //返回数据
+        response.json({ code: 200, msg: "查询详细信息成功", data: result[0] });
+    } catch (err) {
+        response.json({ code: 500, msg: "服务器错误" });
+    }
 })
 
 server.get("/getRoute",async(request,response)=>{
     let url = request.query.url;
-    let result = await route.get(url);
-    console.log(result);
-    response.json(result);
+    let result
+    try{
+        result = await route.get(url);
+
+        //返回数据
+        response.json({ code: 200, msg: "查询路线成功", data: result });
+    } catch (err) {
+        response.json({ code: 500, msg: "服务器错误" });
+    }
 })
 
-//端口号修改到8090！
-server.listen(8090,()=>{
+server.listen(8080,()=>{
     console.log("服务器启动完毕！");
 })
--- core/mysql.js
+++ core/mysql.js
@@ -1,38 +1,29 @@
-//安装mysql模块
-//npm i mysql --save-dev
+const mysql = require("mysql2");
 
-//引入mysql模块
-const mysql = require("mysql");
-
-//创建连接池对象
 const pool = mysql.createPool({
     host: "127.0.0.1",
     user: "root",
-    password: "changshunSQL",
+    password: "Cptbtptp1",
     port: 3306,
-    database:"shenquangu_sql"
+    database: "shenquangu_sql"
 });
 
-//封装sql方法
-module.exports.Pool = pool;
 module.exports.query = (sql, params) => {
-    //获取链接
-    return new Promise((resolve,reject) =>{
-        pool.getConnection((err,conn) => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, conn) => {
             if (err) {
-                console.log("链接mysql失败:"+err.message);
-                pool.releaseConnection();
-            }else{
-                conn.query(sql, params, (err, result, friends) => {
-                    conn.release();
+                console.log("连接MySQL失败:" + err.message);
+                reject(err);
+            } else {
+                conn.query(sql, params, (err, result) => {
+                    conn.release(); // 释放连接
                     if (err) {
-                        console.log("执行sql失败")
+                        console.log("执行SQL失败");
                         reject(err);
-                    }else{
+                    } else {
                         resolve(result);
                     }
-                    //console.log(result);
-                })
+                });
             }
         });
     });
